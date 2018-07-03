2af2c31ac861a3a6b6d3075b2f324b70c839f425
Jessica Long <jessicalong627@gmail.com>
2018-07-03 22:44:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,6 @@
-def prime?(num)
-  if num < 2
-    return false
-  end
-  i = 2
-  while i <= num / 2
-    if num % i == 0
-      return false
-    end
-    i += 1
-  end
-  return true
-end
+ruby def prime?(num) 
+(2...num).each do |d| 
+  return false if num % d == 0 
+  end 
+  true 
+  end 
