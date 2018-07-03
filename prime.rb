f59d06968c17b73ecadc248b172395bbc0d2b829
Jessica Long <jessicalong627@gmail.com>
2018-07-03 22:28:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(n)
   unless n.is_a? Integer
   puts "That's not an integer."
   if n%2 !=0 && n!=2
-    puts "#{n} is prime!"
+    return true
   else
-    puts "#{n} is not prime."
+    return false
   end
 end
